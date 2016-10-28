4dbdf51ed89f8453d72aaad41dd203355ff2b23d
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2016-10-27 23:41:34 -0200

    Novas funcionalidades
    Acertando exibição do item do pedido e colocando formulario de edição de
    dados do cliente

--- src/controllers.php
+++ src/controllers.php
@@ -11,6 +11,8 @@ $app->before(function () use ($app) {
     $app['translator']->addResource('xlf', __DIR__.'/vendor/symfony/validator/Symfony/Component/Validator/Resources/translations/validators/validators.sr_Latn.xlf', 'sr_Latn', 'validators');
 });
 
+$app->mount('/login/redirect', new App\Controller\LoginRedirect());
+
 $app->get('/', function() use ($app) {
     return $app->redirect('/home');
 });
@@ -18,8 +20,8 @@ $app->get('/', function() use ($app) {
 $app->get('/home', function() use ($app) {
     $token = $app['security']->getToken();
     //$sql = "SELECT p.id, p.created_at, it.num_pedidos FROM pedidos p INNER JOIN (SELECT COUNT(id_pedido) , id FROM itens_pedido GROUP BY id_pedido ORDER BY id) it ON p.id = it.id WHERE p.id_cliente = (SELECT id_cliente FROM clientes WHERE email = ?) ORDER BY created_at DESC";
-    $sql = "SELECT id, created_at FROM pedidos WHERE id_cliente = (SELECT id FROM clientes WHERE email = ?) ORDER BY created_at DESC";
-    $pedidos = $app['db']->fetchAll($sql, array((string)$token->getUser()->getUsername()));
+    $sql = "SELECT id, created_at FROM pedidos WHERE id_cliente = ? ORDER BY created_at DESC";
+    $pedidos = $app['db']->fetchAll($sql, array((int)getUserId($app)));
 
     $nomeUsuario = $app['db']->fetchColumn("SELECT nome FROM clientes WHERE email = ?", array((string)$token->getUser()->getUsername()), 0);
 
@@ -58,9 +60,9 @@ $app->post('/insere-item', function(Request $request) use ($app) {
 
     $idPedido = intval($request->get('id_pedido'));
 
-    if(empty($idPedido)) {
+    if(empty($idPedido)) { //se o pedido ainda não existir, eh criado um novo
         $token = $app['security']->getToken();
-        $idCliente = $app['db']->fetchColumn("SELECT id FROM clientes WHERE email = ?", array((string)$token->getUser()->getUsername()), 0);
+        $idCliente = getUserId($app);
         $app['db']->insert('pedidos', array('id_cliente' => $idCliente));
         $idPedido = $app['db']->lastInsertId();
     }
@@ -86,27 +88,43 @@ $app->post('/insere-item', function(Request $request) use ($app) {
 
 });
 
-$app->get('/itens-pedido/{idPedido}', function(Request $request, $idPedido) use ($app) {
+$app->get('/itens-chamado/{idPedido}', function(Request $request, $idPedido) use ($app) {
     $sql = "SELECT ip.id, ip.quantidade, ip.valor, ip.defeito, ip.valor_maodeobra, ip.prazo_entrega, ip.garantia, ip.fatura, ip.recebido_por, ";
     $sql .= "sip.nome AS status_nome, p.produto, ip.created_at FROM itens_pedido ip INNER JOIN status_item_pedido sip ON ip.status = sip.id INNER JOIN produtos p ON ";
-    $sql .= "ip.id_produto = p.id WHERE ip.id_pedido = ? ORDER BY ip.created_at DESC";
+    $sql .= "ip.id_produto = p.id INNER JOIN pedidos o ON o.id = ip.id_pedido WHERE ip.id_pedido = ? AND o.id_cliente = ? ORDER BY ip.created_at DESC";
 
-    $itensPedido = $app['db']->fetchAll($sql, array((int)$idPedido));
+    $itensPedido = $app['db']->fetchAll($sql, array((int)$idPedido, (int)getUserId($app)));
 
     return $app['twig']->render('itens-pedido.html', array(
         'itens_pedido' => $itensPedido,
     ));
 });
 
-$app->mount('/login/redirect', new App\Controller\LoginRedirect());
+$app->get('/visualizar-item/{id}', function(Request $request, $id) use ($app) {
 
-$app->get('/visualizar-pedido/{id}', function(Request $request, $id) use ($app) {
+    $sql = "SELECT ip.quantidade, IF(ip.valor IS NULL, '', ip.valor * ip.quantidade) AS valor_total_item, ";
+    $sql .= "ip.defeito, IF(ip.valor_maodeobra IS NULL, '', ip.valor_maodeobra) AS maodeobra, ";
+    $sql .= "ip.prazo_entrega, IF(ip.garantia IS NULL, '', ip.garantia) AS garantia, ";
+    $sql .= "IF(ip.fatura IS NULL, '', ip.fatura) AS fatura, IF(ip.recebido_por IS NULL, '', ip.recebido_por) AS recebido_por, ";
+    $sql .= "IF(ip.chamado IS NULL, '', ip.chamado) AS chamado, s.nome AS status, p.produto, tp.tipo AS tipo_produto ";
+    $sql .= "FROM itens_pedido ip INNER JOIN status_item_pedido s ON ip.status = s.id INNER JOIN produtos p ON ip.id_produto = p.id ";
+    $sql .= "INNER JOIN tipo_produto tp ON p.tipo_produto = tp.id INNER JOIN pedidos o ON o.id = ip.id_pedido WHERE ip.id = ? AND o.id_cliente = ?";
+
+    $item = $app['db']->fetchAssoc($sql, array((int)$id, (int)getUserId($app)));
+
+    return $app['twig']->render('item-pedido.html', array(
+        'item' => $item,
+    ));
+
+});
+
+$app->get('/visualizar-chamado/{id}', function(Request $request, $id) use ($app) {
 
     $sql = "SELECT * FROM pedidos WHERE id = ?";
     $pedido = $app['db']->fetchAssoc($sql, array((int)$id));
 
-    $sql = "SELECT ip.quantidade, ip.valor, ip.valor_maodeobra, (ip.quantidade * ip.valor + ip.valor_maodeobra) AS valor_total, ip.garantia, ip.fatura, ip.recebido_por, ip.chamado, ip.created_at, s.nome AS status, p.* FROM itens_pedido ip INNER JOIN produtos p ON ip.id_produto = p.id INNER JOIN status_item_pedido s ON ip.status = s.id WHERE ip.id_pedido = ?";
-    $itens_pedido = $app['db']->fetchAll($sql, array((int)$id));
+    $sql = "SELECT ip.id AS id_item, ip.quantidade, ip.valor, ip.valor_maodeobra, (ip.quantidade * ip.valor + ip.valor_maodeobra) AS valor_total, ip.garantia, ip.fatura, ip.recebido_por, ip.chamado, ip.created_at, s.nome AS status, p.* FROM itens_pedido ip INNER JOIN produtos p ON ip.id_produto = p.id INNER JOIN status_item_pedido s ON ip.status = s.id INNER JOIN pedidos o ON o.id = ip.id_pedido WHERE ip.id_pedido = ? AND o.id_cliente = ?";
+    $itens_pedido = $app['db']->fetchAll($sql, array((int)$id, (int)getUserId($app)));
 
     return $app['twig']->render('pedido.html', array(
         'pedido_id' => $id,
@@ -188,6 +206,71 @@ $app->match('/registrar-cliente', function (Request $request) use ($app) {
     return $app['twig']->render('form-bootstrap-anon.html', array('form' => $form->createView()));
 });
 
+$app->match('/alterar-dados', function (Request $request) use ($app) {
+
+    $sql = "SELECT id, bairro, cep, cidade, email, endereco, estado, nome, telefone FROM clientes WHERE id = ?";
+    $cliente = $app['db']->fetchAssoc($sql, array((int)getUserId($app)));
+
+    $form = $app['form.factory']->createBuilder('form', $cliente)
+        ->add('id', 'hidden')
+        ->add('nome')
+        ->add('email', 'email')
+        ->add('senha_atual', 'password')
+        ->add('nova_senha', 'password')
+        ->add('confirmar_senha', 'password')
+        ->add('telefone')
+        ->add('endereco')
+        ->add('bairro')
+        ->add('cidade')
+        ->add('estado', ChoiceType::class, array(
+            'choices' => array("AC"=>"Acre", "AL"=>"Alagoas", "AM"=>"Amazonas", "AP"=>"Amapá","BA"=>"Bahia","CE"=>"Ceará","DF"=>"Distrito Federal","ES"=>"Espírito Santo","GO"=>"Goiás","MA"=>"Maranhão","MT"=>"Mato Grosso","MS"=>"Mato Grosso do Sul","MG"=>"Minas Gerais","PA"=>"Pará","PB"=>"Paraíba","PR"=>"Paraná","PE"=>"Pernambuco","PI"=>"Piauí","RJ"=>"Rio de Janeiro","RN"=>"Rio Grande do Norte","RO"=>"Rondônia","RS"=>"Rio Grande do Sul","RR"=>"Roraima","SC"=>"Santa Catarina","SE"=>"Sergipe","SP"=>"São Paulo","TO"=>"Tocantins")
+        ))
+        ->add('cep')
+        ->getForm();
+
+    #$form->handleRequest($request);
+    if ($request->isMethod('POST')) {
+        $form->bind($request);
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            $user = new AdvancedUser($data['email'], $data['senha_atual']);
+            $encoder = $app['security.encoder_factory']->getEncoder($user);
+            $encodedPassword = $encoder->encodePassword($data['senha_atual'], $user->getSalt());
+
+            $sql = "SELECT COUNT(*) FROM clientes WHERE senha = ? AND id = ?";
+            $numRows = $app['db']->executeQuery($sql, array((string)$encodedPassword, (int)getUserId($app)))->rowCount();
+
+            $app['monolog']->addDebug($encodedPassword . ", " . getUserId($app));
+
+            if(trim($data['confirmar_senha']) != trim($data['nova_senha'])) {
+                $app['session']->getFlashBag()->add('error', 'A nova senha é diferente da confirmação da nova senha!');
+                return $app['twig']->render('form-bootstrap.html', array('form' => $form->createView(), 'titulo' => 'Edite seus dados'));
+            }
+
+            if($numRows != 1) {
+                $app['session']->getFlashBag()->add('error', 'A senha atual não confere!');
+                return $app['twig']->render('form-bootstrap.html', array('form' => $form->createView(), 'titulo' => 'Edite seus dados'));
+            }
+
+            $newUser = new AdvancedUser($data['email'], $data['nova_senha']);
+            $encoderNewUser = $app['security.encoder_factory']->getEncoder($newUser);
+            $newEncodedPassword = $encoder->encodePassword($data['nova_senha'], $newUser->getSalt());
+
+            $app['db']->update('clientes', array(
+                'telefone' => $data['telefone'], 'senha' => $newEncodedPassword,
+                'endereco' => $data['endereco'], 'nome' => $data['nome'], 'email' => $data['email'],
+                'bairro' => $data['bairro'], 'estado' => $data['estado'], 'cep' => $data['cep'], 'cidade' => $data['cidade']), array('id' => $data['id']));
+            
+            $app['session']->getFlashBag()->add('message', 'Seus dados foram atualizados com sucesso!');
+
+        }
+    }
+
+    // display the form
+    return $app['twig']->render('form-bootstrap.html', array('form' => $form->createView(), 'titulo' => 'Edite seus dados'));
+});
+
 /*$app->match('/interno/registrar-produto', function (Request $request) use ($app) {
 
     $tipos_produto = $app['db']->fetchAll('SELECT id, tipo FROM tipo_produto');
@@ -222,3 +305,9 @@ $app->match('/registrar-cliente', function (Request $request) use ($app) {
     return $app['twig']->render('form.html', array('form' => $form->createView()));
 });*/
 
+//funções auxiliares
+
+function getUserId($app) {
+    $token = $app['security']->getToken();
+    return $app['db']->fetchColumn("SELECT id FROM clientes WHERE email = ?", array((string)$token->getUser()->getUsername()), 0);
+}
